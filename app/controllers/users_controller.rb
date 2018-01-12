62ccb36c1ec8e8aa78e585594078c6475d37ea6c
Hoon Lee <yl.hoony@gmail.com>
2018-01-12 17:30:10 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
   #   def logged_in?
   #     !!session[:user_id]
   #   end
-  # 
+  #
   #   def current_user
   #     User.find(session[:user_id])
   #   end
