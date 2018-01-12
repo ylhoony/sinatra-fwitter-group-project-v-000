4f56fa49957b93834949f1147a94d5ffbca038dd
Hoon Lee <yl.hoony@gmail.com>
2018-01-12 17:04:10 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,8 +20,18 @@ class UsersController < ApplicationController
 
   post '/login' do
     user = User.find_by(params[:username])
-    binding.pry
+    if user & user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/'
+    else
 
+  end
+
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
   end
 
 end
