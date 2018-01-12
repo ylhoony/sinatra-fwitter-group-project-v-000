0cb9c318b6a91ea3f96fc11e52954711dfa33a03
Hoon Lee <yl.hoony@gmail.com>
2018-01-12 17:52:07 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
 
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
-    erb :"users/<%= %>"
+    erb :"users/<%=@user.slug%>"
   end
 
 
