4626242b2750c5aeb652589e489056638b7abbb4
Hoon Lee <yl.hoony@gmail.com>
2018-01-12 17:29:08 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,4 +5,17 @@ class TweetsController < ApplicationController
     erb :"views/index"
   end
 
+
+
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+
 end
